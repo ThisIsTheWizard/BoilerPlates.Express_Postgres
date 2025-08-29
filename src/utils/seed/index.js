2df868315e498f13b2e5a18eb918e8b8545c9fb0
Sheikh Ilias Ibn Suruj <55489470+ThisIsTheWizard@users.noreply.github.com>
2025-08-30 03:24:00 +0600

    feat: update environment configuration and improve test setup with seed data

---
+++ src/utils/seed/index.js
@@ -0,0 +1,2 @@
+export { seedAuthTemplates } from 'src/utils/seed/auth-template.seed'
+export { seedRoles } from 'src/utils/seed/role.seed'
--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
 import { expect } from 'chai'
 
+// Seed
+import { seedAuthTemplates, seedRoles } from 'src/utils/seed'
+
 const api = axios.create({
   baseURL: 'http://localhost:8000',
   timeout: 5000
+})
+
+before(async () => {
+  await seedAuthTemplates()
+  await seedRoles()
 })
 
 export { api, expect }
